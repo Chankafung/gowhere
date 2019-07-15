ae65edcdffd6c5cf0b17ffef63cdf98a9f0702b9
Chankafung <982126748@qq.com>
2019-07-15 09:51:26 +0800

    modify misstake

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,198 @@
+<template>
+  <div>
+    <div class="recommend-title">
+      <img src="http://img1.qunarzz.com/piao/fusion/1711/89/ebc329f16c55bb02.png" alt="">
+      <span>猜你喜欢</span>
+    </div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+        <a href="#">
+          <div class="item-img-wrapper">
+            <img class="item-img" :src="item.imgUrl" alt="">
+          </div>
+          <div class="item-content-wrapper">
+            <p>{{item.title}}</p>
+            <div class="item-content-like">
+              <span class="item-like">
+                <strong class="item-like-full" :style="{width:item.width}"></strong>
+                <span class="item-like-grey"></span>
+              </span>
+              <span class="item-discuss">{{item.discuss}}条评论</span>
+            </div>
+            <div class="item-content-price">
+              <span>￥<em>{{item.price}}</em></span>起
+              <span>{{item.area}}</span>
+            </div>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/ea/eaf1bdc38aed9e6690.img.jpg_200x200_3032bf56.jpg',
+          title: '银杏湖乐园',
+          width: '98%',
+          discuss: '3401',
+          price: '94.8',
+          area: '江宁区'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1907/df/dfe043ff2829f50ca3.img.jpg_200x200_6de74461.jpg',
+          title: '南京海底世界',
+          width: '98%',
+          discuss: '7292',
+          price: '100',
+          area: '玄武区'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1904/31/3151f5637e832d86a3.img.jpg_200x200_a19ed206.jpg',
+          title: '南京万达乐园',
+          width: '98%',
+          discuss: '625',
+          price: '138',
+          area: '栖霞区'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/bb/538133c76765880a3d3c802e45015c57.water.jpg_200x200_169b048c.jpg',
+          title: '紫清湖生态温泉',
+          width: '98%',
+          discuss: '1805',
+          price: '107.7',
+          area: '江宁区'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201406/04/4f597aad25208a233999238c65af9b06.jpg_200x200_d1ea2bd2.jpg',
+          title: '南京珍珠泉水上世界',
+          width: '100%',
+          discuss: '673',
+          price: '93.9',
+          area: '珍珠泉'
+        },
+        {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1805/9b/9b3cbcf2624bf8cea3.img.jpg_200x200_075bc9ef.jpg',
+          title: '弘阳未来世界主题乐园',
+          width: '94%',
+          discuss: '1052',
+          price: '51',
+          area: '浦口区'
+        },
+        {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1906/58/588b13f236575746a3.img.jpg_200x200_cb23ae6f.jpg',
+          title: '南京欢乐水魔方',
+          width: '94%',
+          discuss: '2510',
+          price: '180',
+          area: '江宁区'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  /* 声明字体 */
+  @font-face{
+      font-family: 'mpiconfont';
+      src:url('~styles/iconfont/mpiconfont.ttf')
+  }
+  .recommend-title
+    padding: .24rem 0
+    border-top: .2rem solid #f5f5f5
+    img
+      width: .3rem
+      height: .3rem
+      margin-left: .2rem
+      vertical-align: sub
+    span
+      font-size: .32rem
+      line-height: .44rem
+  .item
+    padding: .2rem .24rem
+    a
+      display: block
+      width: 100%
+      height: 100%
+      &:after
+        content: " "
+        display:block
+        height:0
+        visibility:hidden
+        font-size:0
+        clear:both
+        overflow: hidden
+    .item-img-wrapper
+      width: 2rem
+      height: 2rem
+      float: left
+      .item-img
+        width: 100%
+    .item-content-wrapper
+      padding-left: .22rem
+      overflow: hidden
+      p
+        margin-top: .26rem
+        font-size: .32rem
+        height: .44rem;
+        line-height: .44rem
+        color: #333
+        ellipsis()
+      .item-content-like
+        margin-top: .14rem
+        height: .34rem
+        .item-like
+          display: inline-block
+          position: relative
+          width: 1.4rem
+          height: .28rem
+          .item-like-full
+            color: #ffb436
+            font-family: 'mpiconfont'
+            font-size: .28rem
+            position: absolute
+            left: 0
+            z-index: 2
+            overflow: hidden
+          .item-like-grey
+            color: #e0e0e0
+            font-family: 'mpiconfont'
+            font-size: .28rem
+            position: absolute
+            left: 0
+            z-index: 1
+        .item-discuss
+          font-size: .24rem
+          color: #616161
+          vertical-align: top
+          margin-left: .18rem
+          font-size: .24rem
+      .item-content-price
+        margin-top: .22rem
+        font-size: .24rem
+        color: #333
+        position: relative
+        span:nth-child(1)
+          color: #ff8300
+          em
+            font-size: .4rem
+        span:nth-child(2)
+          position: absolute
+          right: 0
+          bottom: 0
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,89 @@
+<template lang="html">
+  <div>
+    <div class="weekend-title">周末去哪儿</div>
+    <div class="weekend-wrapper" v-for="item in weekendList" :key="item.id">
+      <div class="weekend-img">
+        <img :src="item.imgUrl" alt="">
+      </div>
+      <p class="weekend-titletxt">{{item.title}}</p>
+      <p class="weekend-desc">{{item.desc}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/f1/ae9d4299b8aaea.jpg_r_640x214_0c555905.jpg',
+          title: '南京必打卡',
+          desc: '看尽王侯将相荣辱成败，六朝金粉潮起潮落'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/e1/cdbf56d837da59.jpg_r_640x214_105fc3f2.jpg',
+          title: '民国情怀国共风华',
+          desc: '南京，一座历史文化名城，这片土地上曾经经历过太多太多值得我们去追忆的往事'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/f3/d51669250308cc.jpg_r_640x214_7a127cd0.jpg',
+          title: '秦淮金粉里的南京',
+          desc: '欣赏秦淮风光上独具特色的人文景观，夫子庙里深刻体会古代科举制度'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1811/58/a02ad3ab47e3e3.jpg_r_640x214_08977098.jpg',
+          title: '扬州大明寺',
+          desc: '大明寺扬州第一名胜，集佛教庙宇、文物古迹和园林风光于一体的游览胜地'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1603/ff/19b65f7ca38d70.jpg_r_640x214_c9a4a14b.jpg',
+          title: '长江烂漫景',
+          desc: '览滚滚长江，看山花烂漫'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .weekend-title
+    height: .8rem
+    padding-left: .26rem
+    line-height: .8rem
+    color: #212121
+    background-color: #f5f5f5
+  .weekend-wrapper
+    margin-bottom: .1rem
+    background: #fff
+    .weekend-img
+      overflow: hidden
+      height: 0
+      padding-bottom: 37%
+      img
+        width: 100%
+    .weekend-titletxt
+      overflow: hidden
+      color: #212121
+      font-size: .28rem
+      line-height: .48rem
+      padding-left: .2rem
+      margin-top: .14rem
+      ellipsis()
+    .weekend-desc
+      overflow: hidden;
+      color: #616161;
+      font-size: .24rem;
+      line-height: .42rem;
+      padding-left: .2rem
+      padding-bottom: .2rem
+      ellipsis()
+</style>
