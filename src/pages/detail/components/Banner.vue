01c4252e9b5884c7e009a48b66781d40e4eefc56
Chankafung <982126748@qq.com>
2019-08-08 17:16:58 +0800

    banner finish

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,18 +1,37 @@
 <template lang="html">
   <div>
-    <div class="banner">
+    <div class="banner" @click="handleBannerClick">
       <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1511/ea/eaf1bdc38aed9e6690.img.jpg_600x330_5597540f.jpg" alt="">
       <div class="banner-info">
         <div class="banner-title">银杏湖乐园</div>
-        <div class="banner-number"><span class="iconfont">&#xe606;</span>18</div>
+        <div class="banner-number"><span class="iconfont">&#xe606;</span> 18</div>
       </div>
     </div>
+    <gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClick"></gallary>
   </div>
 </template>
 
 <script>
+import Gallary from '../../../common/gallary/Gallary'
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1511/94/94fd49f633a5eb5590.img.jpg_r_800x800_beb76153.jpg', 'http://img1.qunarzz.com/sight/p0/1511/38/384529f35d2c1ff690.img.jpg_r_800x800_e6377ad4.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClick () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    Gallary
+  }
 }
 </script>
 
@@ -33,6 +52,7 @@ export default {
       bottom: 0
       line-height: .6rem
       color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.8));
       .banner-title
         flex: 1
         font-size: .32rem
